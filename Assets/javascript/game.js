a8c02408cdfb0339758b8433256069c6d4edfd7b
Alexis Reyes <Alex@Alexiss-MacBook-Pro-2.local>
2018-12-06 16:42:54 -0600

    Working on the underscore

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,13 +1,13 @@
 //create an array of  Words 
-const word = ['Mud City','Windy City','Chi Town', 'Second City','City of the Big Shoulders','City by the Lake','The City that Works','Heart of America','Chi Raq'];
+const word = ['Mud City', 'Windy City', 'Chi Town', 'Second City', 'City of the Big Shoulders', 'City by the Lake', 'The City that Works', 'Heart of America', 'Chi Raq'];
 //Choose word randomly
-let ranNum = Math.floor(Math.random() * word.length);
-let chooesnWord = word[randNum];
-let underScore = [];
-console.log(chooesnWord);
+let randNum = Math.floor(Math.random() * word.length);
+let choosenWord = word[randNum];
+let underScore = []; 
+console.log(choosenWord);
 //create underscores 
 let generateUnderscore = () => {
-    for(let i = 0; i < chooesnWord.length; i++) {
+    for(let i = 0; i < choosenWord.length; i++) {
         underScore.push('_');
     }
      return underScore;
@@ -15,7 +15,11 @@ let generateUnderscore = () => {
 console.log(generateUnderscore());
 //get users guess
 document.addEventListener('keypress', (event) => {
-    console.log(event);
+    let keyword = String.fromCharCode(event.keyCode);
+    console.log(choosenWord.indexOf(keyword));
+    if(keyword.indexOf(choosenWord) > -1) {
+        console.log(true);
+    }
 });
 //check ig guess is correct
 //if right push to right array
