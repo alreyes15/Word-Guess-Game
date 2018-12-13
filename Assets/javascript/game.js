fcfc0170dfaf764567fc1a30974d54570e38ff31
Alexis Reyes <Alex@Alexiss-MacBook-Pro-2.local>
2018-12-13 11:51:35 -0600

    I talked to steven about an extention, I was in Texas I did not have wifi out in the farm

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,234 +1,45 @@
-// //create an array of  Words 
-// var doubleWord = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o', 'p','q','r','s','t','u','v','w','x','y','z'];
-// var wordBank = ['Mud City', 'Windy City', 'Chi Town', 'Second City', 'City of the Big Shoulders', 'City by the Lake', 'The City that Works', 'Heart of America', 'Chi Raq'];
-// var randNum = Math.floor(Math.random() * word.length);
-// var choosenWord = word[randNum];
-// var rightWord = [];
-// var wrongWord = [];
-// var underScore = []; 
+//create an array of  Words 
+var doubleWord = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o', 'p','q','r','s','t','u','v','w','x','y','z'];
+var wordBank = ['Mud City', 'Windy City', 'Chi Town', 'Second City', 'City of the Big Shoulders', 'City by the Lake', 'The City that Works', 'Heart of America', 'Chi Raq'];
+var randNum = Math.floor(Math.random() * word.length);
+var choosenWord = word[randNum];
+var rightWord = [];
+var wrongWord = [];
+var underScore = []; 
 
-// var docUnderScore = document.getElementsByClassName('underscore');
+var docUnderScore = document.getElementsByClassName('underscore');
 
-// //create underscores 
-// var generateUnderscore = () => {
-//     for(var i = 0; i < choosenWord.length; i++) {
-//         underScore.push('_');
-//         docUnderScore[0].innerHTML = underScore.join(' ');
-//     }
-//      return underScore;
-// }
-
-// // console.log(generateUnderscore());
-// //get users guess
-// document.addEventListener('keypress', (event) => {
-//     var keyword = String.fromCharCode(event.keyCode);
-// //If right
-//     if(choosenWord.indexOf(keyword) > -1) {
-//         // add letter to the array
-//         rightWord.push(keyword);
-//         //replace the underscore with the correct letter
-//         underScore[choosenWord.indexOf(keyword)] = keyword;
-//         //Matching users guesses
-//         if(underScore.join('') == choosenWord) {
-//             alert('You Win'); 
-//         }
-//     }
-//      else{
-//         wrongWord.push(keyword);
-//     }
-// });
-
-//  .innerHTML = generateUnderscore().join('');
-
-// //check if guess is correct
-// //if right push to right array
-// //if wrong push to wrong array
-
-//GLOBAL VARIABLES
-//---------------------------------------
-// Used to record how many times a letter can be pressed
-var doubleWord = ['a','b','c',
-				  'd','e','f',
-				  'g','h','i',
-				  'j','k','l',
-				  'm','n','o',
-				  'p','q','r',
-				  's','t','u',
-				  'v','w','x',
-				  'y','z'];
-//Holds the all the words
-var wordBank =['orange','blue','green', 'yellow','violet','red','purple'];
-//Holds choosenWord
-var choosenWord = "";
-//Holds letters in word
-var lettersInWord = [];
-//Holds number of blanks in word
-var numBlanks = 0;
-//Holds Blanks and successful guesses
-var blanksAndSuccesses =[];
-//Holds Wrong guesses
-var wrongLetters = [];
-//Counters
-var winCount = 0;
-var loseCount = 0;
-var guessesLeft = 9;
-var rightGuessCounter = 0;
-//FUNCTIONS
-//----------------------------------------
-function reset()
-{
-	//Chooses word randombly from the wordBank
-	choosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-	//Splits the choosen word into individual letters
-	lettersInWord = choosenWord.split('');
-	//Get the number of blanks
-	numBlanks = lettersInWord.length;
-	
-	//RESET
-	//===========================================================
-	letterGuessed = 0;
-	rightGuessCounter = 0;
-	guessesLeft = 9;
-	wrongLetters =[];
-	blanksAndSuccesses =[];
-	doubleWord = ['a','b','c',
-					  'd','e','f',
-					  'g','h','i',
-					  'j','k','l',
-					  'm','n','o',
-					  'p','q','r',
-					  's','t','u',
-					  'v','w','x',
-					  'y','z'];
-	test=false;
-	startGame();
-}
-function startGame()
-{
-	//Chooses word randombly from the wordBank
-	choosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-	//Splits the choosen word into individual letters
-	lettersInWord = choosenWord.split('');
-	//Get the number of blanks
-	numBlanks = lettersInWord.length;
-	
-	//RESET
-	//===========================================================
-	rightGuessCounter = 0;
-	guessesLeft = 9;
-	wrongLetters =[];
-	blanksAndSuccesses =[];
-	doubleWord = ['a','b','c',
-					  'd','e','f',
-					  'g','h','i',
-					  'j','k','l',
-					  'm','n','o',
-					  'p','q','r',
-					  's','t','u',
-					  'v','w','x',
-					  'y','z'];
-
-	//Populate blanks
-	for(var i = 0; i< numBlanks; i++)
-	{
-		blanksAndSuccesses.push('_');
-		document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses;
-	}
-
-	//Changes HTML 
-	document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
-	document.getElementById('numGuesses').innerHTML = guessesLeft;
-	document.getElementById('winCounter').innerHTML = winCount;
-	document.getElementById('lossCounter').innerHTML = loseCount;
-	document.getElementById('wrongGuesses').innerHTML = wrongLetters;
-	// Testing / Debugging
-	console.log(choosenWord);
-	console.log(lettersInWord);
-	console.log(numBlanks);
-	console.log(blanksAndSuccesses);
+//create underscores 
+var generateUnderscore = () => {
+    for(var i = 0; i < choosenWord.length; i++) {
+        underScore.push('_');
+        docUnderScore[0].innerHTML = underScore.join(' ');
+    }
+     return underScore;
 }
 
-function compareLetters(userKey)
-{
-				console.log('WORKING!');
-				//If user key exist in choosen word then perform this function 
-				if(choosenWord.indexOf(userKey) > -1)
-				{
-					//Loops depending on the amount of blanks 
-					for(var i = 0; i < numBlanks; i++)
-					{
-						//Fills in right index with user key
-						if(lettersInWord[i] === userKey)
-						{
-							rightGuessCounter++;
-							blanksAndSuccesses[i] = userKey;
-							document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
-						}	
-					}
-					//Test / Debug
-					console.log(blanksAndSuccesses);
-				}
-				//Wrong Keys
-				else
-				{
-					wrongLetters.push(userKey);
-					guessesLeft--;
-					//Changes HTML
-					document.getElementById('numGuesses').innerHTML = guessesLeft;
-					document.getElementById('wrongGuesses').innerHTML = wrongLetters;
-					//Test / Debug
-					console.log('Wrong Letters = ' + wrongLetters);
-					console.log('Guesses left are ' + guessesLeft);
-				}
-			
-			
-		
-}
-function winLose()
-{
-	// When number blanks if filled with right words then you win
-	if(rightGuessCounter === numBlanks)
-	{
-		//Counts Wins 
-		winCount++;
-		//Changes HTML
-		document.getElementById('winCounter').innerHTML = winCount;
-		alert('You Win');
-		reset();
-	}
-	// When number of Guesses reaches 0 then You lose
-	else if(guessesLeft === 0)
-	{
-		//Counts losses
-		loseCount++;
-		//Changes HTML
-		document.getElementById('lossCounter').innerHTML = loseCount;
-		alert('You Lose');
-		reset();
-	}
-}
+// console.log(generateUnderscore());
+//get users guess
+document.addEventListener('keypress', (event) => {
+    var keyword = String.fromCharCode(event.keyCode);
+//If right
+    if(choosenWord.indexOf(keyword) > -1) {
+        // add letter to the array
+        rightWord.push(keyword);
+        //replace the underscore with the correct letter
+        underScore[choosenWord.indexOf(keyword)] = keyword;
+        //Matching users guesses
+        if(underScore.join('') == choosenWord) {
+            alert('You Win'); 
+        }
+    }
+     else{
+        wrongWord.push(keyword);
+    }
+});
 
-//MAIN PROCCESS
-//-------------------------------------------	
-//Initiates the Code
-startGame();
+ .innerHTML = generateUnderscore().join('');
 
-document.onkeyup = function(event)
-{
-	test = true;
-	var letterGuessed = event.key;
-	for(var i = 0; i < doubleWord.length; i++)
-	{	
-		if(letterGuessed === doubleWord[i] && test === true)
-		{
-			var spliceDword = doubleWord.splice(i,1);
-			//Test / Debug
-			console.log('Double word is = ' + doubleWord[i])
-			console.log('Spliced Word is = ' + spliceDword);
-
-			compareLetters(letterGuessed);
-			winLose();
-		}
-	}		
-		
-}
+//check if guess is correct
+//if right push to right array
+//if wrong push to wrong array
