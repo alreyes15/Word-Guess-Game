b4f50204d3b8874663f137b2c9102633ecbce0e4
Alexis Reyes <Alex@alexiss-mbp-2.lan>
2018-12-16 23:31:47 -0600

    Added CSS and Images

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,6 +1,6 @@
 //create an array of  Words 
-var doubleWord = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o', 'p','q','r','s','t','u','v','w','x','y','z'];
-var wordBank = ['Mud City', 'Windy City', 'Chi Town', 'Second City', 'City of the Big Shoulders', 'City by the Lake', 'The City that Works', 'Heart of America', 'Chi Raq'];
+// var doubleWord = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o', 'p','q','r','s','t','u','v','w','x','y','z'];
+var word = ['Mud City', 'Windy City', 'Chi Town', 'Second City', 'City of the Big Shoulders', 'City by the Lake', 'The City that Works', 'Heart of America', 'Chi Raq'];
 var randNum = Math.floor(Math.random() * word.length);
 var choosenWord = word[randNum];
 var rightWord = [];
@@ -8,12 +8,14 @@ var wrongWord = [];
 var underScore = []; 
 
 var docUnderScore = document.getElementsByClassName('underscore');
+var docRightGuess = document.getElementsByClassName('rightGuess');
+var docWrongGuess = document.getElementsByClassName('wrongGuess');
 
-//create underscores 
-var generateUnderscore = () => {
+//create underscores  
+var generateUnderscore = () => { 
     for(var i = 0; i < choosenWord.length; i++) {
         underScore.push('_');
-        docUnderScore[0].innerHTML = underScore.join(' ');
+        
     }
      return underScore;
 }
@@ -26,19 +28,22 @@ document.addEventListener('keypress', (event) => {
     if(choosenWord.indexOf(keyword) > -1) {
         // add letter to the array
         rightWord.push(keyword);
-        //replace the underscore with the correct letter
+
         underScore[choosenWord.indexOf(keyword)] = keyword;
-        //Matching users guesses
-        if(underScore.join('') == choosenWord) {
+        docUnderScore[0].innerHTML = underScore.join(' ');
+        docRightGuess[0].innerHTML = rightWord;
+         
+                if(underScore.join('') == choosenWord) {
             alert('You Win'); 
         }
     }
      else{
         wrongWord.push(keyword);
+        docWrongGuess[0].innerHTML = wrongWord;
     }
 });
 
- .innerHTML = generateUnderscore().join('');
+ docUnderScore[0].innerHTML = generateUnderscore().join(' ');
 
 //check if guess is correct
 //if right push to right array
